2f4675e07fbaa3dfe524a43481841850823586f0
aianman4823 <akitoaianman1@gmail.com>
2019-07-20 09:18:55 +0900

    resolve server stop for interval

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,9 +10,26 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+import requests
+import time
+import threading
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = Cling(get_wsgi_application())
+
+def awake():
+    while True:
+        try:
+            print("Start Awaking")
+            requests.get("https://memo-blog.herokuapp.com/")
+            print("End")
+        except:
+            print("error")
+        time.sleep(300)
+
+t=threading.Thread(target=awake)
+t.start()
+
 
 
